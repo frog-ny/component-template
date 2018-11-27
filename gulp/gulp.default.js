a9339e5b2c82a8adf799b8b30d2dcb7ed09b830a
xy <xy@frogdesign.com>
2018-11-27 18:11:37 -0500

    update sass task

--- gulp/gulp.default.js
+++ gulp/gulp.default.js
@@ -1,7 +1,7 @@
-'use strict';
-var gulp = require('gulp');
-var runSequence = require('run-sequence');
-
-gulp.task('default',function(callback) {
-  runSequence('sass','js','size',callback);
-});
+// 'use strict';
+// var gulp = require('gulp');
+// var runSequence = require('run-sequence');
+//
+// gulp.task('default',function(callback) {
+//   runSequence('sass','js','size',callback);
+// });
--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,6 @@ var gulp = require('gulp');
 var requireDir = require('require-dir');
 
 requireDir('./gulp');
+
+// https://gist.github.com/xtreamwayz/e28c574c466d546d0c6f
+// https://gist.github.com/addyosmani/9f10c555e32a8d06ddb0
